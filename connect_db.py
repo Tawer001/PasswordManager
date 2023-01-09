29c8328f2a0138bbd16b366bd3641feeac9efce6
Tawer001 <tamer.f.mohammed@outlook.com>
2023-01-09 05:28:03 +0300

    This v0.5.2 . Mariadb is replaced by SQLite, which fit my project needs perfectly. You don't have to use a bomb to kill an insect.

--- connect_db.py
+++ connect_db.py
@@ -1,86 +1,24 @@
 # connect_db.py
 import os
-import mysql.connector
-from mysql.connector import Error
+import sqlite3
+from sqlite3 import Error
 import config_db
 
 
-try:
-    # Create a connection pool
-    connection = mysql.connector.connect(
-        host = os.environ['DB_HOST'],
-        user = os.environ['DB_USER'],
-        password = os.environ['DB_PASSWORD'],
-        database = os.environ['DB_DATABASE'],
-        autocommit = True,
-    )
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+def create_connection():
+    """ create a database connection to a database that resides
+        in the memory
+    """
+    conn = None;
+    try:
+        conn = sqlite3.connect(':memory:')
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
-'''
-https://mariadb.com/resources/blog/how-to-connect-python-programs-to-mariadb/
-# Module Imports
-import mariadb
-import sys
-
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="db_user",
-        password="db_user_passwd",
-        host="192.0.2.1",
-        port=3306,
-        database="employees"
-
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-# Get Cursor
-cur = conn.cursor()
-
-
-===================================================
-
-import mysql.connector
-from mysql.connector import Error
-
-try:
-    connection = mysql.connector.connect(
-        host='localhost',
-        database='Electronics',
-        user='pynative',
-        password='pynative@#29'
-        )
-
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
-
-
-
-'''
+if __name__ == '__main__':
+    create_connection()
