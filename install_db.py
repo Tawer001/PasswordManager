a73df3834fcc1665402dcabce42f176e45a4fb76
Tawer001 <tamer.f.mohammed@outlook.com>
2023-01-10 05:40:15 +0300

    v0.7

--- install_db.py
+++ install_db.py
@@ -1,10 +1,7 @@
-# install_db.py
-import subprocess
+#!/usr/bin/python
 
+import sqlite3
 
-def install_mariadb():
-    # Run the MariaDB installer
-    subprocess.check_call(['msiexec', '/i', 'Database/Installation/mariadb-10.11.1-winx64.msi'])
+conn = sqlite3.connect('test.db')
 
-    # Start the MariaDB service
-    subprocess.check_call(['net', 'start', 'mariadb'])
+print("Opened database successfully")
